0c662a2c9c6c643ecc787412bf7ca647f63c4195
Jared Fuller <jaredshane@gmail.com>
2017-03-27 14:07:36 -0500

    commented out my function to try and get out of using the express static

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-const { Router } = require('express')
+const { Router, express } = require('express')
 const router = Router()
 const path = require('path')
 
-
-router.use('/', (req, res, next) => {
-  console.log('hold up')
-  res.sendFile(path.join(__dirname, "../public", 'index.html'))
-})
+// router.get('/', (req, res, next) => {
+//   console.log('hold up')
+//   res.sendFile(path.join(__dirname, "../public", 'index.html'))
+// })
 router.use(require('./games'))
 router.use(require('./hipster'))
 router.use(require('./animals'))
