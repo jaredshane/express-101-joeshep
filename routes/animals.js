230599ae099141fbe01cb8853fb41bd9773aa9ec
Jared Fuller <jaredshane@gmail.com>
2017-03-27 14:59:13 -0500

    modularized /monkeys and /chickens routes

--- routes/animals.js
+++ routes/animals.js
@@ -16,18 +16,13 @@ const animalRouter = Router()
 animalRouter.get('/monkeys', (req, res, next) => {
   console.log('Monkeysssss')
   console.log(`This ran at ${req.requestedTime}`)
-  res.sendFile(path.join(__dirname, '../public', 'monkeys.html'))
-})
-
-animalRouter.get('/chickens', (req, res, next) => {
-  console.log('Look at all these chickens')
-  res.send('<h3>No chickens for you</h3><form method="POST"><input type="text"><button type="submit">push</button></form>')
-})
-
-animalRouter.post('/chickens', (req, res, next) => {
-  console.log('Posting a form for chickens')
-  console.log(req)
+  // res.sendFile(path.join(__dirname, '../public', 'monkeys.html'))
+  animal.new(req, res)
 
 })
+
+animalRouter.get('/chickens', newPage)
+
+animalRouter.post('/chickens', submit)
 
 module.exports = animalRouter
